f3d35bed2cbfdac85774d039fcb7be1f950fc34c
ironlanderl <fabrizio03p@gmail.com>
2025-04-30 21:23:23 +0200

    Aggiunta delle pagine per Dermatologia e Oculistica con specialisti e prestazioni, e aggiornamento delle rotte nel file web.php

---
+++ resources/views/dermatologia.blade.php
@@ -0,0 +1,45 @@
+<?php
+// Simulazione dell'ottenimento dei dati dal database
+$specialisti = [['nome' => 'Dott. Mario', 'cognome' => 'Neri'], ['nome' => 'Dott.ssa Giulia', 'cognome' => 'Gialli'], ['nome' => 'Dott. Paolo', 'cognome' => 'Blu']];
+
+$prestazioni = [['nome' => 'Visita dermatologica', 'descrizione' => 'Visita per la diagnosi di patologie cutanee'], ['nome' => 'Dermatologia pediatrica', 'descrizione' => 'Visita dermatologica per bambini'], ['nome' => 'Trattamento acne', 'descrizione' => 'Trattamento per l\'acne e le sue cicatrici'], ['nome' => 'Controllo nei', 'descrizione' => 'Controllo dei nei e della pelle']];
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poliambulatorio Salus</title>
+    <link rel="stylesheet" href="assets/css/body.css">
+    <link rel="stylesheet" href="assets/css/containers.css">
+    <link rel="stylesheet" href="assets/css/structure.css">
+</head>
+
+<body>
+    <header>
+        <a href="#" class="logo">Dermatologia</a>
+    </header>
+    <main class = "main">
+
+        <h1>Specialisti</h1>
+        <ul>
+            @foreach ($specialisti as $specialista)
+                <li>{{ $specialista['nome'] . ' ' . $specialista['cognome'] }}</li>
+            @endforeach
+        </ul>
+
+
+        <h1>Prestazioni</h1>
+        <ul>
+            @foreach ($prestazioni as $prestazione)
+                <li>{{ $prestazione['nome'] . ': ' . $prestazione['descrizione'] }}</li>
+            @endforeach
+        </ul>
+
+    </main>
+</body>
+
+</html>
