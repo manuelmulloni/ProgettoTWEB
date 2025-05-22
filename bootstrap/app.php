6658544ef31a290106fb18d43fb5431cdcd76c18
ironlanderl <fabrizio03p@gmail.com>
2025-05-22 09:02:29 +0200

    Modifica di alcune funzionalità di breeze per compatibilità con il progetto

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,19 +4,25 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\IsAdmin;
+use Illuminate\Http\Request;
 use Illuminate\Auth\Middleware\Authenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth' => Authenticate::class,
             'isAdmin' => IsAdmin::class,
-        ]);    })
+        ]);
+        // https://laravel.com/docs/12.x/authentication#redirecting-authenticated-users
+        // Di default, se un utente autenticato riprova ad accedere alla pagina di login, viene reindirizzato alla route `dashboard`.
+        // Nel nostro progetto la pagina di default, sia per gli account autenticati che per quelli non autenticati, è la home page.
+        $middleware->redirectUsersTo(fn(Request $request) => route('primaview'));
+    })
 
     ->withExceptions(function (Exceptions $exceptions) {
         //
