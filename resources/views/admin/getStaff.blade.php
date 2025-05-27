362252bd7e48cce24ca31e8dbd0160915df7b81d
mattitom03 <casa@DESKTOP-DENDSEO.station>
2025-05-27 18:29:39 +0200

    visualizzazione membri staff da parte dell'admin

--- resources/views/admin/getStaff.blade.php
+++ resources/views/admin/getStaff.blade.php
@@ -4,6 +4,30 @@
 @section('content')
     <div class = "container">
         <h1>Lista Staff</h1>
+        <div class="content">
+        @if ($utenti->isEmpty())
+            <p>Non è presente nessun mebro dello staff</p>
+        @else
+            <table>
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Nome</th>
+                    <th>Cognome</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($utenti as $utente)
+                    <tr>
+                        <td>{{ $utente->username }}</td>
+                        <td>{{ $utente->nome }}</td>
+                        <td>{{ $utente->cognome }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
 @endsection
 
 
