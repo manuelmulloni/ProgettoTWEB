d90d535b759d37bf7c921b32a5d8f72fe82f2f64
ironlanderl <fabrizio03p@gmail.com>
2025-05-29 13:31:56 +0200

    Unificazione CSS

--- resources/views/layouts/timed_notif.blade.php
+++ resources/views/layouts/timed_notif.blade.php
@@ -1,30 +1,3 @@
-<style>
-    .notification {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        z-index: 1000;
-        padding: 20px;
-        border-radius: 5px;
-    }
-
-    .notification-error {
-        background-color: #f8d7da;
-        color: #721c24;
-        border: 1px solid #f5c6cb;
-    }
-
-    .notification-success {
-        background-color: #d4edda;
-        color: #155724;
-        border: 1px solid #c3e6cb;
-    }
-
-    .notification-close {
-        cursor: pointer;
-    }
-</style>
-
 @if (isset($error) || isset($success))
 
    <div class="notification {{ isset($error) ? 'notification-error' : 'notification-success' }}" hidden id="resultNotification">
