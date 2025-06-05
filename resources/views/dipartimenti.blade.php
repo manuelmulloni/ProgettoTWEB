52ba36cae8b29aefdcb319e0226002e45842b60a
ironlanderl <fabrizio03p@gmail.com>
2025-06-05 11:24:08 +0200

    Fix css dipartimenti admin

--- resources/views/dipartimenti.blade.php
+++ resources/views/dipartimenti.blade.php
@@ -5,36 +5,42 @@
         <h1>Dipartimenti</h1>
         <p>Il nostro poliambulatorio offre una vasta gamma di dipartimenti per soddisfare le tue esigenze sanitarie.</p>
 
-        @if($List->isEmpty())
+        @if ($List->isEmpty())
             <p>Nessun dipartimento disponibile.</p>
         @else
             <ul>
                 @foreach ($List as $dipartimento)
-                    <li>
-                        <a href="{{ route('dipSpec', $dipartimento->id) }}">
-                            {{ $dipartimento->nome }}
-                        </a>
-                        @if(auth()->user() && auth()->user()->livello === 4)
-                        {{-- Pulsante Modifica --}}
-                        <form action="{{ route('dipartimento.modifica', $dipartimento->id) }}" method="GET">
-                            <button type="submit" class="edit-button button-style">Modifica</button>
-                        </form>
+                    <div class="vertical-flex">
+                        {{-- Link al Dipartimento --}}
+                        <div>
+                            <a href="{{ route('dipSpec', $dipartimento->id) }}">
+                                {{ $dipartimento->nome }}
 
-                        {{-- Pulsante Cancella --}}
-                        <form action="{{ route('dipartimento.cancella', $dipartimento->id) }}" method="POST" style="display:inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Sei sicuro di voler cancellare questo dipartimento?')">
-                                Cancella
-                            </button>
-                        </form>
-                    </li>
-                    @endif
+                            </a>
+                        </div>
+                        @if (auth()->user() && auth()->user()->livello === 4)
+                            {{-- Pulsante Modifica --}}
+                            <form action="{{ route('dipartimento.modifica', $dipartimento->id) }}" method="GET">
+                                <button type="submit" class="button-style edit-button">Modifica</button>
+                            </form>
+
+                            {{-- Pulsante Cancella --}}
+                            <form action="{{ route('dipartimento.cancella', $dipartimento->id) }}" method="POST"
+                                style="display:inline-block;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="button-style delete-button"
+                                    onclick="return confirm('Sei sicuro di voler cancellare questo dipartimento?')">
+                                    Cancella
+                                </button>
+                            </form>
+                        @endif
+                    </div>
                 @endforeach
             </ul>
         @endif
 
-        @if(auth()->user() && auth()->user()->livello === 4)
+        @if (auth()->user() && auth()->user()->livello === 4)
             @yield('form')
         @endif
     </div>
