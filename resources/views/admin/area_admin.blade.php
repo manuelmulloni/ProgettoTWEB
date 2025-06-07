5daab92c16a57059ce8207a2fba7a1a36042d4a0
ironlanderl <fabrizio03p@gmail.com>
2025-06-07 09:52:41 +0200

    Fix stile area admin

--- resources/views/admin/area_admin.blade.php
+++ resources/views/admin/area_admin.blade.php
@@ -2,21 +2,20 @@
 
 
 @section('content')
-    <div class="container">
-        <a href = " {{route('prestazioni')}}" class = "button" > Prestazioni</a>
-    </div>
+
 
     <div class="container">
-        <a href = "{{route('dipartimentiAdmin')}}" class = "button" > Dipartimenti</a>
-    </div>
-    <div class="container">
-        <a href = "{{route('getStaff')}}" class = "button" > Gestione ripartizione prestazioni</a>
-    </div>
-    <div class="container">
-        <a href = "{{route('getUsers')}}" class = "button" > Gestione utenti</a>
-    </div>
-    <div class="container">
-        <a href = "{{route('statistiche')}}" class = "button" >Statistiche</a>
+        Bentornato nella tua area personale, {{ Auth::user()->nome }}!
+        <br>
+        Operazioni disponibili:
+        <br>
+        <div class="flex-center">
+            <a href = " {{route('prestazioni')}}"> Prestazioni</a>
+            <a href = "{{route('dipartimentiAdmin')}}" > Dipartimenti</a>
+            <a href = "{{route('getStaff')}}" class = "button" > Gestione ripartizione prestazioni</a>
+            <a href = "{{route('getUsers')}}" class = "button" > Gestione utenti</a>
+            <a href = "{{route('statistiche')}}" class = "button" >Statistiche</a>
+        </div>
     </div>
 @endsection
 
