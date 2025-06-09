a8461ed7041234e2b8546fb7fffc8850dccdb80a
manuelmulloni <95358949+manuelmulloni@users.noreply.github.com>
2025-06-09 10:45:19 +0200

    aggiunti form view

--- resources/views/admin/stats/byPrestazione.blade.php
+++ resources/views/admin/stats/byPrestazione.blade.php
@@ -2,6 +2,21 @@
 @section('content')
     <div class="container">
         <h1>Statistiche per Prestazione</h1>
+        <form method="GET" action="{{ route('stat.Prestazione') }}" class="mb-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="startDate">Scegli data inizio</label>
+                    <input type="date" id="startDate" name="startDate" value="{{ request('startDate') }}" class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <label for="endDate">Scegli data fine</label>
+                    <input type="date" id="endDate" name="endDate" value="{{ request('endDate') }}" class="form-control">
+                </div>
+                <div class="col-md-4 align-self-end">
+                    <button type="submit" class="btn btn-primary">Filtra</button>
+                </div>
+            </div>
+        </form>
         <table class="table table-striped">
             <thead>
             <tr>
